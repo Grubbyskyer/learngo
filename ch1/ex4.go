428fc38f0fda852a441f038368a999d9d4b98618
Grubbyskyer <296065540@qq.com>
2017-05-02 19:18:55 +0800

    Add exercise1.4 code.

---
+++ ch1/ex4.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		counts[line]++
+		if counts[line] > 1 {
+			fmt.Printf("filename: %v, dup line: %s, counts: %d\n", f.Name(), line, counts[line])
+		}
+	}
+}
