48f7059b3d250d4b868f12a8b5fd316f27a3ad60
Grubbyskyer <296065540@qq.com>
2017-05-03 22:01:32 +0800

    Add exercise2.5 code.

---
+++ ch2/ex5.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func main() {
+	var i uint64
+	fmt.Println("using single exp:")
+	start := time.Now()
+	for i = 0; i < 1e8; i++ {
+		PopCount1(i)
+	}
+	fmt.Printf("time cost: %.2fs\n", time.Since(start).Seconds())
+	fmt.Println("using x&(x-1):")
+	start = time.Now()
+	for i = 0; i < 1e8; i++ {
+		PopCount2(i)
+	}
+	fmt.Printf("time cost: %.2fs\n", time.Since(start).Seconds())
+}
+
+func PopCount1(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCount2(x uint64) int {
+	var count int
+	for x != 0 {
+		x = x & (x - 1)
+	}
+	return count
+}
